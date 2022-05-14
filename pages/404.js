4ddd1af67dc4d10434e454e06f763ec96f30e206
Mathis H <mathish@macbook-pro-mathis-travail.home>
2022-05-14 16:43:38 +0200

    error page is done

---
+++ pages/404.js
@@ -0,0 +1,27 @@
+import Wrapper from '../wrappers/ErrorPage';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Custom404() {
+  return (
+    <Wrapper className='full-page'>
+      <div>
+        <div style={{ width: '100%', maxWidth: '600px', marginBottom: '2rem' }}>
+          <Image
+            src='/not-found.svg'
+            width='100%'
+            height='100%'
+            layout='responsive'
+            alt='error'
+          />
+        </div>
+
+        <h3>Ohh! Page Not Found</h3>
+        <p>We can't seem to find the page you're looking for</p>
+        <Link href='/' passHref>
+          <a>back home</a>
+        </Link>
+      </div>
+    </Wrapper>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import DefaultLayout from '../layouts/DefaultLayout';
 export default function Home() {
   return (
     <DefaultLayout>
-      <h1>hello world</h1>
+      <h1>Under construction</h1>
     </DefaultLayout>
   );
 }
